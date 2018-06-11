a976f99b304d90e839897b48b6d286162b091d48
Aleksi Leino <aleksi.leino@gmail.com>
2018-06-11 15:23:15 +0300

    Help for createMedium

--- createMedium.m
+++ createMedium.m
@@ -1,60 +1,68 @@
-function vmcmedium_out = createMedium(vmcmesh, vmcmedium_in)
+function vmcmedium_out = createMedium(vmcmesh, vmcmedium)
+%CREATEMEDIUM Creates a 'vmcmedium' structure array
 %
-% Complete a 'vmcmedium' -structure for a given geometry
+% DESCRIPTION:
+%        
+%       Creates a 'vmcmedium' structure array (see https://inverselight.github.io/ValoMC/structures.html)
+%       or turns the existing fields in a given medium into arrays that are compatible in size with a
+%       given mesh.
+%    
+% USAGE:
+%       vmcmedium = createMedium(vmcmesh);
 %
-% vmcmedium_out = createMedium(vmcmesh, vmcmedium_in)
+%           returns the following structure
+%    
+%                   vmcmedium.refractive_index(:) = 1;               
+%                   vmcmedium.scattering_coefficient(:) = 0; 
+%                   vmcmedium.absorption_coefficient(:) = 0; 
+%                   vmcmedium.scattering_anisotropy(:) = 0;
 %
+%           The size of each array is equal to the number of elements
+%           in the mesh.
 %
-% INPUT
+%       vmcmedium_out = createMedium(vmcmesh, vmcmedium);
 %
-%  vmcmesh:  	    mesh structure (see documentation/structure reference)
+%           returns the structure as above but each existing field in
+%           vmcmedium is copied to vmcmedium_out by repeating it until
+%           the size matches the number of elements in the mesh
 %
-% OPTIONAL INPUT
+% INPUTS:
+%       vmcmesh       -  see https://inverselight.github.io/ValoMC/structures.html  
 %
-%  vmcmedium_in:	existing medium structure (see documentation/structure reference)
-%               where the array size does not match the number of elements in
-%               the mesh.
-% OUTPUT
+% OPTIONAL INPUTS:
+%       vmcmedium     -  incomplete structure array for medium (see https://inverselight.github.io/ValoMC/structures.html)
 %
-%  vmcmedium_out:  vmcmedium structure for a given mesh. The array sizes match
-%               the number of elements in the mesh.
-%               If 'vmcmedium_in' was given as a input, the values for the arrays in
-%               'vmcmedium_out' are obtained by repeating 'vmcmedium_in'. 
-%               If no 'vmcmedium_in' was given, the function returns a structure with
-%               the fields formatted as follows:
-%
-%                   vmcmedium_out(:).refractive_index = 1;               
-%                   vmcmedium_out(:).scattering_coefficient = 0; [1/mm]
-%                   vmcmedium_out(:).absorption_coefficient = 0; [1/mm]
-%                   mediun_out(:).scattering_anisotropy = 0;
-%
-%
-    if(~exist('vmcmedium_in'))
-         vmcmedium_in.refractive_index = 1;               
-         vmcmedium_in.scattering_coefficient = 0;
-         vmcmedium_in.absorption_coefficient = 0; 
-         vmcmedium_in.scattering_anisotropy = 0;        
-    end
-    vmcmedium_out = vmcmedium_in;
+% OUTPUTS:
+%       vmcmedium_out -  structure array for medium
 
-    if(~isfield(vmcmedium_in, 'refractive_index'))
-       vmcmedium_in.refractive_index = 1;
+    if(~exist('vmcmedium'))
+         vmcmedium.refractive_index = 1;               
+         vmcmedium.scattering_coefficient = 0;
+         vmcmedium.absorption_coefficient = 0; 
+         vmcmedium.scattering_anisotropy = 0;        
+    end
+    vmcmedium_out = vmcmedium;
+
+    if(~isfield(vmcmedium, 'refractive_index'))
+       vmcmedium.refractive_index = 1;
     end;
-    if(~isfield(vmcmedium_in, 'scattering_coefficient'))
-         vmcmedium_in.scattering_coefficient = 0.0; 
+    if(~isfield(vmcmedium, 'scattering_coefficient'))
+         vmcmedium.scattering_coefficient = 0.0; 
     end;
-    if(~isfield(vmcmedium_in, 'absorption_coefficient'))
-         vmcmedium_in.absorption_coefficient = 0.0;
+    if(~isfield(vmcmedium, 'absorption_coefficient'))
+         vmcmedium.absorption_coefficient = 0.0;
     end;
-    if(~isfield(vmcmedium_in, 'scattering_anisotropy'))
-         vmcmedium_in.absorption_coefficient = 0.0; 
+    if(~isfield(vmcmedium, 'scattering_anisotropy'))
+         vmcmedium.absorption_coefficient = 0.0; 
     end;
     
-    if(~isvector(vmcmedium_in.refractive_index)) 
-        vmcmedium_out.nx = size(vmcmedium_in.refractive_index,1);
-        vmcmedium_out.ny = size(vmcmedium_in.refractive_index,2);
-        if(size(vmcmedium_in.refractive_index, 3)>1)
-		vmcmedium_out.nz = size(vmcmedium_in.refractive_index,3);        
+    % check if the optical coefficients are given as multidimensional arrays
+
+    if(~isvector(vmcmedium.refractive_index)) 
+        vmcmedium_out.nx = size(vmcmedium.refractive_index,1);
+        vmcmedium_out.ny = size(vmcmedium.refractive_index,2);
+        if(size(vmcmedium.refractive_index, 3)>1)
+		vmcmedium_out.nz = size(vmcmedium.refractive_index,3);        
            if(vmcmedium_out.nx*vmcmedium_out.ny*vmcmedium_out.nz ~= length(vmcmesh.H)/6)
                warning('Mesh size and medium size do not seem compatible.');
            end
@@ -65,12 +73,12 @@ function vmcmedium_out = createMedium(vmcmesh, vmcmedium_in)
         end
     end
     
-    if(~isvector(vmcmedium_in.scattering_coefficient))
-        vmcmedium_out.nx = size(vmcmedium_in.scattering_coefficient,1);
-        vmcmedium_out.ny = size(vmcmedium_in.scattering_coefficient,2);
+    if(~isvector(vmcmedium.scattering_coefficient))
+        vmcmedium_out.nx = size(vmcmedium.scattering_coefficient,1);
+        vmcmedium_out.ny = size(vmcmedium.scattering_coefficient,2);
         
-        if(size(vmcmedium_in.scattering_coefficient, 3)>1)
-		vmcmedium_out.nz = size(vmcmedium_in.scattering_coefficient,3);        
+        if(size(vmcmedium.scattering_coefficient, 3)>1)
+		vmcmedium_out.nz = size(vmcmedium.scattering_coefficient,3);        
            if(vmcmedium_out.nx*vmcmedium_out.ny*vmcmedium_out.nz ~= length(vmcmesh.H)/6)
                warning('Mesh size and medium size do not seem compatible.');
            end
@@ -81,11 +89,11 @@ function vmcmedium_out = createMedium(vmcmesh, vmcmedium_in)
         end
     end
     
-    if(~isvector(vmcmedium_in.absorption_coefficient))
-        vmcmedium_out.nx = size(vmcmedium_in.absorption_coefficient,1);
-        vmcmedium_out.ny = size(vmcmedium_in.absorption_coefficient,2);
-        if(size(vmcmedium_in.absorption_coefficient, 3)>1)
-           vmcmedium_out.nz = size(vmcmedium_in.absorption_coefficient,3);
+    if(~isvector(vmcmedium.absorption_coefficient))
+        vmcmedium_out.nx = size(vmcmedium.absorption_coefficient,1);
+        vmcmedium_out.ny = size(vmcmedium.absorption_coefficient,2);
+        if(size(vmcmedium.absorption_coefficient, 3)>1)
+           vmcmedium_out.nz = size(vmcmedium.absorption_coefficient,3);
            if(vmcmedium_out.nx*vmcmedium_out.ny*vmcmedium_out.nz ~= length(vmcmesh.H)/6)
                warning('Mesh size and medium size do not seem compatible.');
            end
@@ -96,11 +104,11 @@ function vmcmedium_out = createMedium(vmcmesh, vmcmedium_in)
         end
     end
     
-    if(~isvector(vmcmedium_in.scattering_anisotropy))
-        vmcmedium_out.nx = size(vmcmedium_in.scattering_anisotropy,1);
-        vmcmedium_out.ny = size(vmcmedium_in.scattering_anisotropy,2);
-        if(size(vmcmedium_in.scattering_anisotropy, 3)>1)
-           vmcmedium_out.nz = size(vmcmedium_in.scattering_anisotropy,3);
+    if(~isvector(vmcmedium.scattering_anisotropy))
+        vmcmedium_out.nx = size(vmcmedium.scattering_anisotropy,1);
+        vmcmedium_out.ny = size(vmcmedium.scattering_anisotropy,2);
+        if(size(vmcmedium.scattering_anisotropy, 3)>1)
+           vmcmedium_out.nz = size(vmcmedium.scattering_anisotropy,3);
            if(vmcmedium_out.nx*vmcmedium_out.ny*vmcmedium_out.nz ~= length(vmcmesh.H)/6)
                warning('Mesh size and medium size do not seem compatible.');
            end
@@ -112,9 +120,9 @@ function vmcmedium_out = createMedium(vmcmesh, vmcmedium_in)
     end
 
 
-    vmcmedium_out.refractive_index = duplicateArray(vmcmedium_in.refractive_index(:), size(vmcmesh.H,1));
-    vmcmedium_out.scattering_coefficient = duplicateArray(vmcmedium_in.scattering_coefficient(:), size(vmcmesh.H,1));
-    vmcmedium_out.absorption_coefficient = duplicateArray(vmcmedium_in.absorption_coefficient(:), size(vmcmesh.H,1));
-    vmcmedium_out.scattering_anisotropy = duplicateArray(vmcmedium_in.scattering_anisotropy(:), size(vmcmesh.H,1));
+    vmcmedium_out.refractive_index = duplicateArray(vmcmedium.refractive_index(:), size(vmcmesh.H,1));
+    vmcmedium_out.scattering_coefficient = duplicateArray(vmcmedium.scattering_coefficient(:), size(vmcmesh.H,1));
+    vmcmedium_out.absorption_coefficient = duplicateArray(vmcmedium.absorption_coefficient(:), size(vmcmesh.H,1));
+    vmcmedium_out.scattering_anisotropy = duplicateArray(vmcmedium.scattering_anisotropy(:), size(vmcmesh.H,1));
    
 end

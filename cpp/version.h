50d3db01aa09ecf2fcffae3f1e8fcfa5281c9023
Aleksi Leino <aleksi.leino@gmail.com>
2018-07-04 18:48:22 +0300

    createBH3

--- cpp/version.h
+++ cpp/version.h
@@ -1,4 +1,4 @@
-const char * build_version = "v1.0b";
-const char * build_revision = "71";
+const char * build_version = "v1.0b-1-g8110e8b";
+const char * build_revision = "72";
 
 
--- createBH.m
+++ createBH.m
@@ -22,33 +22,33 @@ function [BH, BMother] = createBH(H, HN);
 %                     boundary element belongs to
 
 if(size(H,2) > 3)
-   if(exist('createBH3') == 0)
-       error('Cannot find mex function createBH3. It is located at cpp/3d/createBH3mex.cpp. See installation instructions how to compile it');
-   end
-   if(nargout > 1)
-     error('Cannot create BMother for 3d meshes yet.');
-   end
-   BH = createBH3(int64(H));
+    if(exist('createBH3') == 0)
+        error('Cannot find mex function createBH3. It is located at cpp/3d/createBH3mex.cpp. See installation instructions how to compile it');
+    end
+    if(nargout > 1)
+        error('Cannot create BMother for 3d meshes yet.');
+    end
+    BH = createBH3(int64(H));
 
 else
 
-[els, edge] = find(HN == 0);
+    [els, edge] = find(HN == 0);
 
-BH = zeros(numel(els), 2);
-BMother = zeros(size(BH, 1), 1);
+    BH = zeros(numel(els), 2);
+    BMother = zeros(size(BH, 1), 1);
 
-for(el = 1 : length(BH))
-  if(edge(el) == 1)
-    BH(el, :) = H( els(el), [1 2]);
-    BMother(el) = els(el);
-  elseif(edge(el) == 2)
-    BH(el, :) = H( els(el), [2 3]);
-    BMother(el) = els(el);
-  elseif(edge(el) == 3)
-    BH(el, :) = H( els(el), [3 1]);
-    BMother(el) = els(el);
-  end
-end;
+    for(el = 1 : length(BH))
+        if(edge(el) == 1)
+            BH(el, :) = H( els(el), [1 2]);
+            BMother(el) = els(el);
+        elseif(edge(el) == 2)
+            BH(el, :) = H( els(el), [2 3]);
+            BMother(el) = els(el);
+        elseif(edge(el) == 3)
+            BH(el, :) = H( els(el), [3 1]);
+            BMother(el) = els(el);
+        end
+    end;
 
 %BH=sortBH(BH);
 
